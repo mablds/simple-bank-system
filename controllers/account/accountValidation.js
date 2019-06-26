4d8f8f7f556ee245621f1b002161922e0bcb3216
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-06-26 17:01:29 -0300

    /bank /register = OK

--- controllers/account/accountValidation.js
+++ controllers/account/accountValidation.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose')
 const express = require('express'),
     router = express.Router()
 
-router.get('/', async(req, res) => {
-    const verifyAccount = Account.find({ titular: req.body.titular })
-    console.log(verifyAccount)
+router.post('/', async(req, res) => {
+    const verifyAccount = await (Account.find({ titular: req.body.titular, senha: req.body.senha }))
+        // console.log(verifyAccount)
+    if (verifyAccount[0].titular === req.body.titular) {
+        console.log(verifyAccount)
+    } else {
+        console.log('non ecsiste')
+    }
 })
 
 
