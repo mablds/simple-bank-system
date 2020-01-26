f33b0b5554622f6892c2b74a0b5ac4536bb37e97
Marcelo Arthur <marcelobragalemos@gmail.com>
2020-01-26 16:56:08 -0300

    bodyparser removed. express implemented this func

--- config/config.js
+++ config/config.js
@@ -1,22 +1,12 @@
+require('dotenv').config()
 const express = require('express')
-const path = require('path')
-const pug = require('pug')
+const cors = require('cors')
 
 module.exports.setUpServer = () => {
     const app = express()
-    const bodyParser = require('body-parser')
 
-    app.set('view engine', 'pug')
-    app.set("views", path.join(__dirname, "views"))
-
-    const loggerMiddleware = require('../middlewares/logger')
-        // app.use(loggerMiddleware)
-
-    app.use("/static", express.static(path.join(__dirname, "public")))
-
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: true }))
-
+    app.use(express.json())
+    app.use(cors())
     app.use(require('../controllers'))
 
     return app
