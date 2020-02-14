0367eabcea820aa4a18248b8131c65003fd3db8b
Marcelo Arthur <marcelobragalemos@gmail.com>
2020-02-14 01:35:12 -0300

    token authentication middleware added

--- controllers/account/accountHandler.js
+++ controllers/account/accountHandler.js
@@ -8,14 +8,17 @@ const createAccount = require('./createAccount')
 const updateAccount = require('./updateAccount.js')
 const deleteAccount = require('./deleteAccount.js')
 
+//Middleware functions
+const adminAccess = require('../../middlewares/adminAccess')
+const auth = require('../../middlewares/auth')
+
 //Routes and Middlewares
-router.get('/', getAllAccounts.getAll)
-router.get('/:account', getAccount.getOne)
+router.get('/', adminAccess, getAllAccounts.getAll)
+router.get('/:account', auth, getAccount.getOne)
 router.post('/create', createAccount)
 router.post('/deposit', updateAccount.deposit)
-router.post('/withdraw', updateAccount.withdraw)
-router.put('/transfer', updateAccount.trasnfer)
-router.delete('/delete', deleteAccount)
-
+router.post('/withdraw', auth, updateAccount.withdraw)
+router.put('/transfer', auth, updateAccount.trasnfer)
+router.delete('/delete', adminAccess, deleteAccount)
 
 module.exports = router
