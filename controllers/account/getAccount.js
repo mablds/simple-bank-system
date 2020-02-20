1bb6a0b77e8cb68e40784b1ee442987b5da86204
MARCELO ARTHUR BRAGA LEMOS DOS SANTOS <marcelo.ablsantos@senacsp.edu.br>
2020-02-20 19:36:03 -0300

    HTTP status code fixed

--- controllers/account/getAccount.js
+++ controllers/account/getAccount.js
@@ -1,11 +1,11 @@
 const Account = require('../../models/account-model')
 
 module.exports.getOne = async(req, res) => {
-    if (!req.params.account) res.send('Parâmetros vazios. Informe uma conta válida.')
+    if (!req.params.account) res.status(400).send('Parâmetros vazios. Informe uma conta válida.')
     try {
         const account = await Account.findOne({ account: req.params.account })
-        res.send(account)
+        res.status(200).send(account)
     } catch (err) {
-        res.send(err)
+        res.status(500).send(err)
     }
 }
