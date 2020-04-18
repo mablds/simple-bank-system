7c89acdcd2fe42da67bc4fba1ef9c59d83cd66ff
mablds <marcelobragalemos@gmail.com>
2020-04-18 02:17:15 -0300

    login method updated

--- controllers/auth/verifAccount.js
+++ controllers/auth/verifAccount.js
@@ -9,24 +9,22 @@ module.exports.findByAccount = async(req, res) => {
         })
     }
     if (req.body) {
-        if(!req.body.account) res.status(204).json({ msg: 'Favor informar uma conta válida' })
-        //if(!req.body.email) res.status(204).json({ msg: 'Favor informar uma email válido' })
-
-        const account = await Account.find({ account: req.body.account })
-        console.log(account)
-        if (req.body.password === account[0].password) {
-            res.status(200).json({
-                id: account[0]._id,
-                owner: account[0].owner,
-                value: 'R$ ' + account[0].value,
-                admin: account[0].admin,
-                token: generateToken({ id: account[0]._id, admin: account[0].admin })
-            })
-        } else {
-            res.status(403).json({ msg: 'senha incorreta' })
-        }
-        
+        //if(!req.body.account) res.status(204).json({ msg: 'Favor informar uma conta válida' })
+        if(!req.body.email) res.status(204).json({ msg: 'Favor informar uma email válido' })
+        Account.find({ email: `${req.body.email}`}).then((account) => {
+            if (req.body.password === account[0].password) {
+                res.status(200).json({
+                    id: account[0]._id,
+                    owner: account[0].owner,
+                    value: 'R$ ' + account[0].value,
+                    admin: account[0].admin,
+                    token: generateToken({ id: account[0]._id, admin: account[0].admin })
+                })
+            } else {
+                res.status(403).json({ msg: 'senha incorreta' })
+            }
+        })
     } else {
-        res.status(204).json({ msg: 'Body vazio' })
+        res.status(204).json({ msg: 'Body inválido' })
     }
 }
